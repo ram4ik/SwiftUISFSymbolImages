cb941d68d720b4c4600a51169f759d77f2eb7533
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-01-20 10:35:42 +0200

    screenshots

--- SwiftUISFSymbolImages/ContentView.swift
+++ SwiftUISFSymbolImages/ContentView.swift
@@ -9,8 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var modify: Bool = false
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [
+                self.modify ? Color.orange : Color.red,
+                self.modify ? Color.yellow : Color.orange
+            ]), startPoint:
+                self.modify ? .bottomLeading : .bottomTrailing,
+                endPoint:
+                self.modify ? .topTrailing : .topLeading)
+                .edgesIgnoringSafeArea(.all)
+                
+            Image(systemName: self.modify ? "cloud.rain.fill" : "sun.max.fill")
+                .font(.system(size: 100))
+                .foregroundColor(self.modify ? .blue : .red)
+                .shadow(color: self.modify ? .blue : .red, radius: 5, x: 5, y: 10)
+                .padding(100)
+                .background(
+                    Circle()
+                        .foregroundColor(.orange)
+                        .opacity(0.7))
+                .rotationEffect(.degrees(self.modify ? 360 : 0))
+        }.animation(.default)
+        .onTapGesture {
+            self.modify.toggle()
+        }
     }
 }
 
